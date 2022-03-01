e21ded5a8eb4c81f443476417c2b2d2fd9caf18a
sradical <rsdsplace@gmail.com>
2022-03-01 15:35:47 -0800

    testing git from pycharm

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import transformers
 from transformers import AutoTokenizer, AutoModel, BertTokenizer, BertModel, AutoTokenizer, AutoModelForMaskedLM
 # #Kaggle_TRAINING_FILE = "../../../MLMedicalNotesNLPChallenge/bert-entity-extraction/ner_dataset.csv"
 NCBI_TRAINING_FILE = '../input/NCBITraining_tagged.csv'
-#
+
 MAX_LEN = 128 # Padding length
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
