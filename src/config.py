be673e36b7fc73f94cd5dd6e416b41aae0ea3c75
sradical <rsdsplace@gmail.com>
2022-03-01 15:48:01 -0800

    testing git from pycharm

--- src/config.py
+++ src/config.py
@@ -7,7 +7,6 @@ MAX_LEN = 128 # Padding length
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-
 TOKENIZER =  AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT") # Clinical Bert
 MODEL_PATH = './model.bin'
 
