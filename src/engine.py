594e8efeff977fe6216c6e37080edc0ead7bfd76
sradical <rsdsplace@gmail.com>
2022-03-10 22:30:51 -0800

    Fixed loops

--- src/engine.py
+++ src/engine.py
@@ -2,30 +2,28 @@ import torch
 from tqdm import tqdm
 
 def train_fn(data_loader, model, optimizer, device, scheduler):
-    print(f"Training Device {device}")
     model.train()
     final_loss = 0
-    for data in tqdm(data_loader, total = len(data_loader)):
+    for data in tqdm(data_loader, total=len(data_loader)):
         for k, v in data.items():
             data[k] = v.to(device)
-            _, _, loss = model(**data)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-            final_loss += loss.item()
+        optimizer.zero_grad()
+        _, _, loss = model(**data)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        final_loss += loss.item()
     return final_loss / len(data_loader)
 
 def eval_fn(data_loader, model, device):
     model.eval()
     final_loss = 0
-    for data in tqdm(data_loader, total = len(data_loader)):
+    for data in tqdm(data_loader, total=len(data_loader)):
         for k, v in data.items():
             data[k] = v.to(device)
-            _, _, loss = model(**data)
-            final_loss += loss
+        _, _, loss = model(**data)
+        final_loss += loss.item()
     return final_loss / len(data_loader)
-
 
 
 
