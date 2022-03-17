cd43304448cb22d59283db5c6c224f0b1b179f55
sradical <rsdsplace@gmail.com>
2022-03-17 14:24:14 -0700

    Added Kaggle dataset file

--- src/config.py
+++ src/config.py
@@ -1,12 +1,15 @@
 import torch
 import transformers
+import spacy
 from transformers import AutoTokenizer, AutoModel, BertTokenizer, BertModel, AutoTokenizer, AutoModelForMaskedLM
 NCBI_TRAINING_FILE = '../input/NCBITraining_tagged.pkl'
+Kaggle_TRAINING_FILE = '../input/KaggleNERDataset.pkl'
 
 MAX_LEN = 128 # Padding length
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
-TOKENIZER =  AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT") # Clinical Bert
-MODEL_PATH = './model.bin'
-
+#TOKENIZER =  AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT") # Clinical Bert
+TOKENIZER = BertTokenizer.from_pretrained("bert-base-uncased")
+#MODEL_PATH = './model.bin'
+MODEL_PATH = './kaggle_model.bin'
