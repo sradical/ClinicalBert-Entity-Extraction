574f70663bc1da546e9efa3546df3369a7fb59df
sradical <rsdsplace@gmail.com>
2022-03-18 13:00:46 -0700

    Added model name to config to generalize code

--- src/config.py
+++ src/config.py
@@ -2,20 +2,23 @@ import torch
 import transformers
 import spacy
 from transformers import AutoTokenizer, AutoModel, BertTokenizer, BertModel, AutoTokenizer, AutoModelForMaskedLM
-NCBI_TRAINING_FILE = '../input/NCBITraining_tagged.pkl'
-Kaggle_TRAINING_FILE = '../input/KaggleNERDataset.pkl'
+
 
 MAX_LEN = 128 # Padding length
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 8
 EPOCHS = 10
 
+# DATAFILES
+NCBI_TRAINING_FILE = '../input/NCBITraining_tagged.pkl'
+Kaggle_TRAINING_FILE = '../input/KaggleNERDataset.pkl'
+
 # Clinical Bert
-BASE_MODEL = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT", output_hidden_states=True, return_dict=False)
-TOKENIZER =  AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-MODEL_PATH = './model.bin'
+#BASE_MODEL = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT", output_hidden_states=True, return_dict=False)
+#TOKENIZER =  AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+#MODEL_PATH = './model.bin'
 
 # Bert-Base-uncased
-#transformer_model = BertModel.from_pretrained("bert-base-uncased")
-#TOKENIZER = BertTokenizer.from_pretrained("bert-base-uncased")
-#MODEL_PATH = './kaggle_model.bin'
+BASE_MODEL = BertModel.from_pretrained("bert-base-uncased")
+TOKENIZER = BertTokenizer.from_pretrained("bert-base-uncased")
+MODEL_PATH = './kaggle_model.bin'
